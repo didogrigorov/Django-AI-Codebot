1af62107c1ffd0cf4e3ee7bbcc9a8c6469c22f63
Dilyan Grigorov <dido.grigorov@gmail.com>
2023-11-08 23:43:48 +0200

    Update settings.py

--- codebot/settings.py
+++ codebot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-be=a!%&f##txz8*8-@0!^l@57law_nq5hwf4m4vxrvhdky^+k@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
